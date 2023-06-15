bbbe6b5dc43c227580b3421260238c875a1834b8
MRosama90 <mrizk15903@gmail.com>
2023-06-15 03:05:46 -0700

    checking task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
-#!/bin/bash
-int main(void )
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: c program that prints using put
+ * Return: Always 0 
+*/
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

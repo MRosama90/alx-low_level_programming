99f8e1588fd3c063cc18ffc223b7e0493e920ac4
MRosama90 <mrizk15903@gmail.com>
2023-06-20 02:21:50 -0700

    checking task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
+
 /**
  * print_sign - print + if n > zero,
  * 0 if n = zero - and if n < zero
  * @n: takes integer input for functions
- * Return: 1 if +,
- * 0 if 0,
- * -1 if -
+ * Return: 1 if +, 0 if 0, -1 if -
 */
 
 int print_sign(int n)

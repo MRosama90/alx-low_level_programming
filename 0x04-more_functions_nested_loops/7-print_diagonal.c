454915cae2f67657fe12f8a64b68caffffc31439
MRosama90 <mrizk15903@gmail.com>
2023-06-22 13:43:50 -0700

    ??????????????????????????????

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,21 +3,22 @@
 /**
  * print_diagonal - print a digonal line
  * @n: is the number of times the \ character should be printed
+ * Return: void
 */
 
 void print_diagonal(int n)
 {
-	int postn, space;
+	int a, b;
 
 	if (n <= 0)
 		_putchar('\n');
-	for (postn = 0; postn < n; postn++)
+	for (a = 0; i < n; i++)
 	{
-		for (space = 0; space < postn; space++)
+		for (b = 0; b < a; b++)
 		{
 			_putchar(' ');
 		}
-		_putchar('92');
+		_putchar("92");
 		_putchar('\n');
 	}
 }

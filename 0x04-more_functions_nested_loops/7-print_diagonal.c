f3c2b561e4f4b2b464bc54b0afb9bb106f20f9dc
MRosama90 <mrizk15903@gmail.com>
2023-06-22 13:31:47 -0700

    checking task 8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,9 +11,9 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-	for (postn = 1; postn <= n; postn++)
+	for (postn = 0; postn < n; postn++)
 	{
-		for (space = 1; space <= postn; space++)
+		for (space = 0; space < postn; space++)
 		{
 			_putchar(' ');
 		}

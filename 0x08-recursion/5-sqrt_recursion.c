31cf6c36970ec245d269d9d59deb0ce23b4d3d5c
MRosama90 <mrizk15903@gmail.com>
2023-07-05 01:39:02 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - find natural square root
+ * @n: int
+ * @val: square root
+ * Return: int
+ */
+
+int square(int n, int val);
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+/**
+ * square - find square root
+ * @n: int to find sqaure root
+ * @val: square root.
+ * Return: int
+ */
+
+int sqaure(int n, int val)
+{
+	if (val  * val == n)
+		return (val);
+	else if (val * val < n)
+		return (sqaure(n, val + 1));
+	else
+		return (-1);
+}

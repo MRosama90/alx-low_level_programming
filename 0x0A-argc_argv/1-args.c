64af5af7c763a444e928e1f2e10dec1fb4d01f7a
MRosama90 <mrizk15903@gmail.com>
2023-08-06 00:39:17 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - asdasda
+ * @argc: asdasda
+ * @argv: asdasd
+ * Return: adasdad
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}

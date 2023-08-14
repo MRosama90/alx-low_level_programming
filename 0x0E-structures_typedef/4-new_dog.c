df57ca431b996050dc2f1a96a3ac0d3f1e15df96
MRosama90 <mrizk15903@gmail.com>
2023-08-14 07:53:16 -0700

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strlen - a asdasd
+ * @str: adasd
+ * Return: adasd
+ */
+
+int _strlen(const char *str)
+{
+	int length = 0;
+
+	while (*str++)
+		length++;
+	return (length);
+}
+
+/**
+ * _strcopy - asdas
+ * @src: asdasd
+ * @dest: asdasd
+ * Return: asdasd
+ */
+
+char *_strcopy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - asda
+ * @name: asdas
+ * @age: asdasd
+ * @owner: asdasd
+ * Return: asdasd
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	if (!name || age < 0 || !owner)
+		return (NULL);
+
+	dog = (dog_t *) malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if ((*dog).name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = _strcopy(dog->name, name);
+	dog->age = age;
+	dog->owner = _strcopy(dog->owner, owner);
+
+	return (dog);
+}

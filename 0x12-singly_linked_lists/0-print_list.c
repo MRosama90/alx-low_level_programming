7ac26e5b682a17a0eab1478df81a60afc4dbf930
MRosama90 <mrizk15903@gmail.com>
2023-08-25 00:37:30 -0700

    asdasd

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * _strlen - asdasd
+ * @s: asdasd
+ * Return: asdasd
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - asdasd
+ * @h: asdasd
+ * Return: adasd
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}

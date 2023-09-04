76762437e71ec96f7b06e7dc0da1b8cc681b5411
MRosama90 <mrizk15903@gmail.com>
2023-09-04 05:16:09 -0700

    asdasd

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strlen - asdasd
+ * @s: asdasd
+ * Return: asdasd
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - asdasd
+ * @filename: asdasd
+ * @text_content: asdasd
+ * Return: asdasd
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_TRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}

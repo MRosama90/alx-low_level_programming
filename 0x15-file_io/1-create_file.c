6762fc28ba7a0023151e592634899087b849d46f
MRosama90 <mrizk15903@gmail.com>
2023-10-03 01:15:33 -0700

    asdasd

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,16 +9,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int a;
-	ssize_t b = 0, l = _strlen(text_content);
+	int fd, a, l = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	a = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (a == -1)
+
+	if (text_content != NULL)
+	{
+		for (l = 0; text_content[l];)
+			l++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	a = write(fd, text_content, l);
+
+	if (fd == -1 || a == -1)
 		return (-1);
-	if (l)
-		b = write(a, text_content, l);
-	close(a);
-	return (b == l ? 1 : -1);
+
+	close(fd);
+
+	return (1);
 }
